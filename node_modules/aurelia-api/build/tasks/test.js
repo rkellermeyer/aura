2a9ae1ab55c0665afa141c8da09a2a10266b7b81
= <joel.cox.dev@gmail.com>
2016-08-09 20:30:17 -0700

    azure

---
+++ node_modules/aurelia-api/build/tasks/test.js
@@ -0,0 +1,44 @@
+var gulp        = require('gulp');
+var KarmaServer = require('karma').Server;
+var server      = require('./server');
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', ['lint'], function(done) {
+  server.start(function() {
+    var karmaServer = new KarmaServer({
+      configFile: __dirname + '/../../karma.conf.js',
+      singleRun: true
+    }, function(exitCode) {
+      server.stop(function() {
+        done();
+
+        process.exit(exitCode);
+      });
+    });
+
+    karmaServer.start();
+  });
+});
+
+/**
+ * Watch for file changes and re-run tests on each change
+ */
+gulp.task('tdd',  function(done) {
+  server.start(function() {
+    var karmaServer = new KarmaServer({
+      configFile: __dirname + '/../../karma.conf.js',
+      singleRun: false,
+      browsers: ['Chrome']
+    }, function(exitCode) {
+      server.stop(function() {
+        done();
+
+        process.exit(exitCode);
+      });
+    });
+
+    karmaServer.start();
+  });
+});
