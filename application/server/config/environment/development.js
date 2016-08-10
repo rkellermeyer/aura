26c4a92a51b4b1fd15361fe2aec236e27c0bc903
= <joel.cox.dev@gmail.com>
2016-08-09 19:27:12 -0700

    update mongo-uri

--- application/server/config/environment/development.js
+++ application/server/config/environment/development.js
@@ -6,10 +6,11 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/idyuh'
+    uri: 'mongodb://joelcoxokc:091190@Dev@ds042729.mlab.com:42729/joelcox-db'
   },
 
   // Seed database on startup
   seedDB: true
 
 };
+// mongo ds050087.mlab.com:50087/clurtch-data -u joelcoxokc -p 091190@Dev
